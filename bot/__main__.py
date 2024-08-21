097e5aa834d98facb0e11f06e247cdf67319ba9f
Mikhail Toshkin <toshkin.mikhail@yandex.ru>
2024-08-21 12:39:11 +0000

    Добавлении нстроек логирования и локализации, также добавлены команды start, help

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,9 +1,25 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
+
+from fluent.runtime import FluentLocalization, FluentResourceLoader
+from pathlib import Path
 from bot.config import settings
 
 async def main():
 
+    # Настройка логирования
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", 
+    )
+
+    # Локализация 
+    locales_path = Path(__file__).parent.joinpath("locales")
+    l10n_loader = FluentResourceLoader(str(locales_path) + "/{locale}")
+    l10n = FluentLocalization(["en", "ru"], ["strings.ftl", "errors.ftl"], l10n_loader)
+
+    
     bot = Bot(token=settings.bot_token.get_secret_value())
     dp = Dispatcher()
 
--- bot/config.py
+++ bot/config.py
@@ -1,11 +1,12 @@
 from pydantic import BaseSettings, SecretStr
 
+
 class Settings(BaseSettings):
     bot_token: SecretStr
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-        env_nested_delimitier = "__"
+
 
 settings = Settings()
